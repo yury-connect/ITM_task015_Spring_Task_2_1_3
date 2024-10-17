2646fd99ac88fe09d1645b123f0ad4ae2c692cf7
Yury_Home-PC_Win10_worked <yury.connect@gmail.com>
2024-10-17 23:54:39 +0300

    solution: Осталось разобраться с п.4

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,12 +1,21 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
+//    @Bean(name="bean")
+//    public Timer getTimer() {
+//        Timer timer = new Timer();
+//        return timer;
+//    }
+
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
 
     public void whatAnimalSay() {
